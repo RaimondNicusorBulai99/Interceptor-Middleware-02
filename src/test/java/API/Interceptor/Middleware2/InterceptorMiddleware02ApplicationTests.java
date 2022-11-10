f97b605a6aac698625afb4dc5a97d8bf63389773
RaimondnicusorBulai <Condivisionedatiraimond@gmail.com>
2022-11-10 10:14:47 +0100

    Initial commit

---
+++ src/test/java/API/Interceptor/Middleware2/InterceptorMiddleware02ApplicationTests.java
@@ -0,0 +1,13 @@
+package API.Interceptor.Middleware2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterceptorMiddleware02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
